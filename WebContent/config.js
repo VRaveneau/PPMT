5534539881325483fcfe29a6ef93370ea32c364a
Vincent Raveneau <vincent.raveneau@univ-nantes.fr>
2018-02-24 23:19:33 +0100

    Fix a typo in the config

--- WebContent/config.js
+++ WebContent/config.js
@@ -4,4 +4,4 @@ var config = {
 	websocketAdress : "ppmt.univ-nantes.fr/ppmt/wsppmt",
 	// Adress of the servlet that provides the dataset list
 	servletAdress : "http://ppmt.univ-nantes.fr/ppmt/datasetProvider"
-};Z
+};
