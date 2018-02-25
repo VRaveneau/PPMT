27e03de48ff01006d51a11e64e5d16247303b48e
Vincent Raveneau <vincent.raveneau@univ-nantes.fr>
2018-02-25 01:08:08 +0100

    Add information about the behavior of WebContent/config.js during merges

--- WebContent/config.js
+++ WebContent/config.js
@@ -1,4 +1,11 @@
-// Configuration
+ /**
+ * Configuration for the js part of the project.
+ * Made branch-specific in the way described at 
+ * https://git-scm.com/book/en/v2/Customizing-Git-Git-Attributes#_merge_strategies.
+ * 
+ * Even though this file is version-controled, it is ignored during merges.
+ * If content is added to it, a separate commit must be made in all branches of the project 
+ */
 var config = {
 	// Adress of the websocket that we want to connect to
 	websocketAdress : "ppmt.univ-nantes.fr/ppmt/wsppmt",
