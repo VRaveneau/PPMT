8347c4eb577dff52a529a7825136b7db9211321f
Vincent Raveneau <vincent.raveneau@univ-nantes.fr>
2020-12-17 00:15:04 +0100

    Add license

--- WebContent/ServerInterface.js
+++ WebContent/ServerInterface.js
@@ -1,3 +1,19 @@
+
+/* This file is copyright (c) 20015-2020 Vincent Raveneau
+* 
+* This file is part of the PPMT software.
+* 
+* PPMT is free software: you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation, either version 3 of the License, or (at your option) any later
+* version.
+* PPMT is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with
+* PPMT. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * Describes a server interface and its methods. It is used to abstract the
  * interaction with the server from the main code, to provide several
